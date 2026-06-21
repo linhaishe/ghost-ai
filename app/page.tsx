becf0f9607ad3124141b857b926bbdbf22b9c53f
miamiamia <42030043+linhaishe@users.noreply.github.com>
2026-06-21 16:46:02 +0800

    feat: Feature 01 finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="flex min-h-screen items-center justify-center">ghost Al</div>;
+  return (
+    <>
+      <div className="flex justify-center">
+        ghost Alerytryr
+      </div>
+      <Button>test</Button>
+    </>
+  );
 }
